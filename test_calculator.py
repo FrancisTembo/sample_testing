d3b0f1acc2a4d31dae11733d2aa529b3e52468d0
Francis Tembo <36688983@student.g.nwu.ac.za>
2022-01-22 22:29:51 +0200

    Added test for subtract function

--- test_calculator.py
+++ test_calculator.py
@@ -1,6 +1,6 @@
 import pytest
 
-from calculator import add, multiply, divide
+from calculator import add, multiply, divide, subtract
 
 
 @pytest.mark.parametrize("a, b, c", [(1, 1, 2), (10, 5, 15), (4, 5, 9)])
@@ -16,3 +16,8 @@ def test_multiply(a, b, c):
 @pytest.mark.parametrize("a, b, c", [(1, 1, 1), (10, 5, 2), (40, 5, 8)])
 def test_divide(a, b, c):
     assert divide(a, b) == c
+
+
+@pytest.mark.parametrize("a, b, c", [(1, 1, 0), (10, 5, 5), (40, 5, 35)])
+def test_subtract(a, b, c):
+    assert subtract(a, b) == c
